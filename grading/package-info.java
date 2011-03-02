c638176caadf25e5faa4f63af2c2147461ed4fc1
Steven Karas <steven.karas@gmail.com>
2011-03-02 19:46:12 +0200

    Added package-info.java

---
+++ grading/package-info.java
@@ -0,0 +1,29 @@
+/**
+ * package-info.java
+ * 
+ * Copyright (C) 2010-2011
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * See README for contact information. See LICENSE for GPL license
+ */
+
+package grading;
+
+/**
+ * In this package, we provide multiple utility classes and systems that help to write JUnit tests
+ * to check Java-language homework assignments
+ * 
+ * @author Steven Karas
+ * @author Idan Felix
+ */
